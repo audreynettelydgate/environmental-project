d8787c8f3e6f567b5f6a1b1481134a227a97c9ae
audreynette.lydgate <audreynette.lydgate@gmail.com>
2026-03-18 21:24:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ function increasePlastic(){
 function saveOcean(){
   document.getElementById("saved").innerText = "🌊 You made a difference!";
 }
+
+const slider = document.getElementById("slider");
+const afterImage = document.getElementById("afterImage");
+
+if(slider){
+  slider.addEventListener("input", function(){
+    let value = slider.value;
+    afterImage.style.clipPath = `inset(0 ${100 - value}% 0 0)`;
+  });
+}
